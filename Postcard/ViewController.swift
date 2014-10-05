ceea1e5afe249d4d2f6780aeb9a44afb084411bc
Ricardo Sandy <ricardosnr@RDS-Snrs-MB.local>
2014-10-05 17:00:59 -0400

    challenge 2 to test commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     @IBOutlet weak var enterNameTextField: UITextField!
     
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -35,6 +37,11 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         mailbutton.setTitle("Mail Sent", forState: UIControlState.Normal)
